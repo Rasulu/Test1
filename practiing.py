b172e9ca23e59412bb0243c41ca01fc647c5b08e
Rasulu <56083526+Rasulu@users.noreply.github.com>
2019-10-02 10:25:35 -0500

    Added new py file

---
+++ practiing.py
@@ -0,0 +1,22 @@
+# import unittest
+# from selenium import webdriver
+
+# class (unittest.TestCase):
+# 	def setUp(self):
+# 		self.driver = webdriver.chrome("C:\Auto_QA\chromedriver")
+# 	def newpage(self):
+# 		self.driver.get("www.google.com")
+# 		self.driver.implicitly_wait(10)
+# 	def teardown(self):
+# 		self.driver.quit()
+
+# if __name__ = __main__
+# unittest.main()
+
+
+
+
+for a in [2,4,5,9,6]:
+	add = a+2
+	print (add)
+print ("Done")
